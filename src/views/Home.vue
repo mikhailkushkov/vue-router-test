52c93d01f149714fa66623f05c12a65a04e43892
Mikhail Kushkov <mikhailkushkov@gmail.com>
2018-12-01 18:12:26 +0100

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,22 @@
 <template>
   <div class="home page">
-    <h1>This is an home page</h1>
-    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
-      dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
-      clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
-      sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
-      At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
-      ipsum dolor sit amet.</p>
+    <h1>This is a home page</h1>
+    <UsersList v-bind:users="users"></UsersList>
   </div>
 </template>
+
+<script>
+import UsersList from '../components/UsersList.vue';
+export default {
+  props: [
+    'users'
+  ],
+  components: {
+    UsersList
+  }
+};
+</script>
+
 
 <style scoped lang="less">
   .home {
