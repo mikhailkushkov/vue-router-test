ed35213a9d026c2d9361f6961e26995750201971
Mikhail Kushkov <mikhailkushkov@gmail.com>
2018-12-01 18:11:17 +0100

    Update router.js

--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
+import About from './views/About.vue';
+import Contact from './views/Contact.vue';
 
 Vue.use(Router)
 
@@ -26,14 +28,6 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "contact" */ './views/Contact.vue'),
-    },
-    {
-      path: '/testLink',
-      name: 'testLink',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "testLink" */ './views/testLink.vue'),
-    },
+    }
   ],
 });
